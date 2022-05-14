12922a7e354a95ba1b224c8217bee68f59a5dd1d
Pooja Kelkar <poojakelkar53@gmail.com>
2022-05-14 13:47:00 +0530

    deleted some files

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,39 +1,10 @@
 import React from "react";
-import { AiFillDelete, AiFillEdit } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { mintuesToHoursAndMinutes } from "../../utils/time";
 import "./card.css";
 
-const Card = ({ taskobj, index, deleteTask, openEditModal }) => {
-    const handleDelete = () => {
-        deleteTask(index);
-    };
+const Card = () => {
     return (
         <div className='card-wrapper'>
-            <Link
-                to={`/task/${index}`}
-                style={{ textDecoration: "none", width: "100%" }}>
-                <div className='card-holder'>
-                    <h5 className='card-heading'>{taskobj.name}</h5>
-                    <h7 className='card-timer'>
-                        {mintuesToHoursAndMinutes(taskobj.time)}
-                    </h7>
-                </div>
-            </Link>
-            <div className='footer'>
-                <AiFillEdit
-                    size={20}
-                    className='icon'
-                    style={{ color: "#4f3cf9", marginRight: "20" }}
-                    onClick={() => openEditModal(index)}
-                />
-                <AiFillDelete
-                    size={20}
-                    className='icon'
-                    style={{ color: "#4f3cf9" }}
-                    onClick={handleDelete}
-                />
-            </div>
+            <h1>card component</h1>
         </div>
     );
 };
--- src/modals/EditTask.js
+++ src/modals/EditTask.js
@@ -1,79 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import React from "react";
 
-const EditTask = ({ modal, toggle, updateTask, taskobj }) => {
-    const [taskName, setTaskName] = useState("");
-    const [desc, setDesc] = useState("");
-    const [timer, setTimer] = useState("");
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        if (name === "taskName") {
-            setTaskName(value);
-        } else if (name === "taskDescription") {
-            setDesc(value);
-        } else {
-            setTimer(value);
-        }
-    };
-
-    useEffect(() => {
-        setTaskName(taskobj.name);
-        setDesc(taskobj.description);
-        setTimer(taskobj.time);
-    }, [taskobj]);
-
-    const handleEdit = (e) => {
-        e.preventDefault();
-        let tempObj = {};
-        tempObj["name"] = taskName;
-        tempObj["description"] = desc;
-        tempObj["time"] = timer;
-        updateTask(tempObj);
-    };
+const EditTask = () => {
     return (
-        <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader>Edit Task</ModalHeader>
-            <ModalBody>
-                <form>
-                    <div className='form-group'>
-                        <label>Task Name</label>
-                        <input
-                            type='text'
-                            className='form-control'
-                            value={taskName}
-                            name='taskName'
-                            onChange={handleChange}></input>
-                    </div>
-                    <div className='form-group'>
-                        <label>Task Description</label>
-                        <textarea
-                            rows='5'
-                            className='form-control'
-                            value={desc}
-                            name='taskDescription'
-                            onChange={handleChange}></textarea>
-                    </div>
-                    <div className='form-group'>
-                        <label>Task Time (In Minutes)</label>
-                        <input
-                            type='number'
-                            className='form-control'
-                            value={timer}
-                            name='taskTimer'
-                            onChange={handleChange}></input>
-                    </div>
-                </form>
-            </ModalBody>
-            <ModalFooter>
-                <Button color='primary' onClick={handleEdit}>
-                    Save
-                </Button>
-                <Button color='secondary' onClick={toggle}>
-                    Cancel
-                </Button>
-            </ModalFooter>
-        </Modal>
+        <div>
+            <h1>Edit task Model</h1>
+        </div>
     );
 };
 
